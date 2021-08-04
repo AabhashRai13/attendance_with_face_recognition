55c1fd277777dedc5cb22ff9a32e69d659877ef0
aabhash <aabhashrai@prixatech.com>
2021-08-04 03:07:31 -0700

    token saved, logout implement and checking logged in status during app run

--- lib/app/modules/home/views/login.dart
+++ lib/app/modules/home/views/login.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:attendance_project/app/modules/home/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -17,28 +20,26 @@ class _LoginPageState extends State<LoginPage> {
   bool validate = false;
   String name1, password1;
   var _response;
-  
-  alertbox(String message){
-    return
-      showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder:(_)=> AlertDialog(
-          title: Text("Error"),
-          content: Text(
-              message),
-          actions: [
-            // ignore: deprecated_member_use
-            FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                }, child: Text("Okay"))
-          ],
-        ),
-      );
+
+  alertbox(String message) {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) => AlertDialog(
+        title: Text("Error"),
+        content: Text(message),
+        actions: [
+          // ignore: deprecated_member_use
+          FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Okay"))
+        ],
+      ),
+    );
   }
-  
-  
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -77,11 +78,9 @@ class _LoginPageState extends State<LoginPage> {
                             borderRadius: BorderRadius.circular(20.0),
                           ),
                           errorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                            borderSide: BorderSide(color: Colors.red)
-                          ),
-                          hintText: "Name"
-                      ),
+                              borderRadius: BorderRadius.circular(20.0),
+                              borderSide: BorderSide(color: Colors.red)),
+                          hintText: "Name"),
                       validator: (String value) {
                         if (value.isEmpty) {
                           return 'Name is Required';
@@ -102,11 +101,10 @@ class _LoginPageState extends State<LoginPage> {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        hintText: "Password"
-                      ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          hintText: "Password"),
                       validator: (String value) {
                         if (value.isEmpty) {
                           return 'Password is Required';
@@ -120,61 +118,72 @@ class _LoginPageState extends State<LoginPage> {
                     SizedBox(
                       height: 20.0,
                     ),
-                    
+
                     // ignore: deprecated_member_use
                     RaisedButton(
                         color: Colors.green,
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                        child: showLoading
+                            ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : Text(
+                                'LOGIN',
+                                style: TextStyle(color: Colors.white),
+                              ),
                         onPressed: () async {
                           setState(() {
                             showLoading = true;
                           });
-                          
+
                           if (_key.currentState.validate()) {
                             _key.currentState.save();
-                            
+
                             Map<String, String> data = {
                               "name": name2.text,
                               "password": password2.text,
                             };
-                            
-                            try{
+
+                            try {
                               _response = await http.post(
-                              "https://protected-chamber-11354.herokuapp.com/users/authenticate",
+                                  "https://protected-chamber-11354.herokuapp.com/users/authenticate",
                                   body: data);
-                            }catch(e){
+                            } catch (e) {
                               setState(() {
-                                showLoading=false;
+                                showLoading = false;
                               });
                               alertbox("Please Check Internet Connection");
-                              
                             }
                             print(_response.statusCode);
-                            print(_response);
+                            Map response = jsonDecode(_response.body);
+                            String authToken = response["data"]["token"];
+                            print(authToken);
+
                             if (_response.statusCode == 200) {
                               setState(() {
                                 validate = false;
                               });
+                              SharedPreferences prefs =
+                                  await SharedPreferences.getInstance();
+                              prefs.setString('isLoggedIn', "loggedIn");
+                              prefs.setString('authToken', authToken);
                               Navigator.pushReplacement(
                                   context,
                                   MaterialPageRoute(
                                       builder: (_) => HomeView()));
-                            }
-                            else {
+                            } else {
                               alertbox("Please enter valid Email and Password");
                               setState(() {
-                                showLoading=false;
+                                showLoading = false;
                               });
                             }
                           } else {
                             setState(() {
                               showLoading = false;
                             });
-
                           }
+                          setState(() {
+                            showLoading = false;
+                          });
                         })
                   ],
                 ),
